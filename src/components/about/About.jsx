98c84eeced40a8d01f659625eaec0d7b050149ed
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-08 20:24:18 +0800

    Extra details / info in the about section ongoing

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
 import "./about.css";
+import Me from "../../assets/mimic4.jpg";
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="about container section" id='about'>
+      <h2 className="section__title">About Me</h2>
+
+      <div className="about__container grid">
+        <img src={Me} alt="" className='about__img home__img' />
+
+        <div className="about__data grid">
+
+          <div className="about__info">
+            <p className="about__description">Hello! I&apos;m Gio Majadas, an IT student particularly interested in making innovations and pontifications about technology. I have a passion for coding and problem solving. Also have an strong background</p>
+            <a href="" className="btn">Download CV</a>
+          </div>
+
+          <div className="about__skills grid">
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Front-End Development</h3>
+                <span className="skills__number">72%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage skill-1"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Database Management</h3>
+                <span className="skills__number">68%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage skill-2"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Server-side Development</h3>
+                <span className="skills__number">54%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage skill-3"></span>
+              </div>
+            </div>
+            
+          </div>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
