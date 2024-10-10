e959e8a2a781ed064dea92f7707d3ac42ce41d17
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-10 14:18:58 +0800

    Resume updated, but need to rewrite the infos in education and experience

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,52 @@
+const Data = [
+    {
+        id: 1,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2010 - 2015",
+        title: "Elementary",
+        desc: "Elementary school was a thrilling adventure filled with curiosity, adventure, and shared activities. Students focused on mastering basic abilities and creating a sense of wonder for their surroundings, while finding my inner self what am I when I grow up continuing to excel in my studies.",
+    },
+    {
+        id: 2,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2015 - 2021",
+        title: "Junior - Senior High School",
+        desc: "Junior and Senior high school marked a significant period of independence and self-discovery, allowing for extensive study of various topics, social changes, and the development of personal interests and strengths. It also provided opportunities for professional growth and technical skill enhancement.",
+    },
+    {
+        id: 3,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2023 - Current",
+        title: "College",
+        desc: "Undergraduate years in information technology focused on programming, database administration, network security, and cloud computing, enhancing technical skills, communication, and technological awareness through internships, projects, and coding competitions.",
+    },
+    {
+        id: 4,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2022",
+        title: "Intern at Google",
+        desc: "Assisted in the development of a web application using ReactJS. Also collaborated with a team of developers to resolve bugs and improve code quality; and gained experience with some project methodologies and version control using Git",
+    },
+    {
+        id: 5,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2021",
+        title: "Personal Project: Chatbot Development",
+        desc: "Designed and developed a conversational chatbot using ReactJS, Firebase and Django. Implemented natural language processing (NLP) techniques to improve chatbot responses and deployed the chatbot on a cloud platform, ensuring scalability and reliability.",
+    },
+    {
+        id: 6,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2020 - 2021",
+        title: "IT Support for Local Non-Profit",
+        desc: "Provided technical support to staff and clients, troubleshooting hardware and software issues, and providing advice and guidance. Also assisted in the setup and maintenance of computer systems and networks.",
+    },
+];
+
+export default Data
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
 import "./resume.css";
+import Data from "./Data";
+import Card from "./Card";
 
 const Resume = () => {
   return (
     <section className="resume container section" id="resume">
-      <h2 className="section__title" data-aos="fade-right">Resume</h2>
+      <h2 className="section__title" data-aos="fade-right">Experience</h2>
+      <div className="resume__container grid">
+        <div className="timeline grid">
+          {Data.map((val, id) => {
+            if (val.category === "education") {
+              return (
+                <Card
+                  key={id}
+                  icon={val.icon}
+                  title={val.title}
+                  year={val.year}
+                  desc={val.desc}
+                />
+              ) 
+            }
+          })}
+        </div>
+        <div className="timeline grid">
+          {Data.map((val, index) => {
+            if (val.category === "experience") {
+              return(
+                <Card
+                  key={index}
+                  icon={val.icon}
+                  title={val.title}
+                  year={val.year}
+                  desc={val.desc}
+                />
+              ) 
+            }
+          })}
+        </div>
+      </div>
     </section>
   )
 }
