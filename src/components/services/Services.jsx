e729d28a7c36d75a6ea836df5584d72c8280182c
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-10 14:19:49 +0800

    Added an logo

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,24 +1,24 @@
-// import Image1 from "../../assets/service-1.svg";
-// import Image2 from "../../assets/service-2.svg";
-// import Image3 from "../../assets/service-3.svg";
 import "./services.css";
 
 const data = [
   {
     id: 1,
+    icon: "fas fa-code",
     title: "Full-Stack Development",
     description: "Designing and developing the client-side using React and other libraries and frameworks, while handling the server-side using Python Django. Also integrating with databases and APIs",
   },
   {
     id: 2,
+    icon: "fas fa-database",
     title: "Database Management",
     description: "Designing, implementing, and maintaining databases to store and manage data efficiently while ensuring data security, integrity, and backup and recovery",
   },
   {
     id: 3,
+    icon: "fas fa-mobile",
     title: "Mobile App Developer",
     description: "Designed and implemented user interfaces and user experience using React Native, while developing and maintaining mobile applications for Android and iOS platforms",
-  }
+  },
 ];
 
 const Services = () => {
@@ -26,9 +26,10 @@ const Services = () => {
     <section className="services container section" id="services">
       <h2 className="section__title" data-aos="fade-right">Services</h2>
       <div className="services__container grid">
-        {data.map(({ id, title, description }) => {
+        {data.map(({ id, icon, title, description }) => {
           return (
             <div className="services__card" key={id}>
+              <i className={icon}></i>
               <h3 className="services__title">{title}</h3>
               <p className="services__description">{description}</p>
             </div>
