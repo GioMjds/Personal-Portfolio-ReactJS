c440bf4a88ad247cfcf01d9c8061d4dd3207dc15
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-10 16:45:53 +0800

    Added contact page content. Working on functionality in email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+
 import "./contact.css";
 
 const Contact = () => {
   return (
     <section className="contact container section" id='contact'>
-      <h2 className="section__title" data-aos="fade-right">Contact Me</h2>
+      <h2 className="section__title" data-aos="fade-right">How to Contact Me?</h2>
+
+      <div className="contact__container grid">
+        <div className="contact__info">
+          <h3 className="contact__title">Let&apos;s talk about business!</h3>
+          <p className="contact__details">Don&apos;t like forms? Send me an Email 👋</p>
+        </div>
+
+        <form action="" className="contact__form">
+          <div className="contact__form-group">
+            <div className="contact__form-div">
+              <input
+                type="text"
+                className="contact__form-input"
+                required placeholder="Your Name"
+              />
+            </div>
+
+            <div className="contact__form-div">
+              <input
+                type="email"
+                className="contact__form-input"
+                required placeholder="Your Email"
+              />
+            </div>
+          </div>
+
+            <div className="contact__form-div contact__form-area">
+              <input
+                type="text"
+                className="contact__form-input"
+                required placeholder="Your Subject"
+              />
+            </div>
+
+            <div className="contact__form-div">
+              <textarea
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                className="contact__form-input"
+                placeholder="Write your message"
+              ></textarea>
+            </div>
+            <button className="btn">Send Message</button>
+        </form>
+      </div>
     </section>
   )
 }
