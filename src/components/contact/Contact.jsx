768687819ee41fcc69c5d984170698d4c42707ff
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-10 18:57:43 +0800

    Installed CORS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,28 @@
-
+import { useState } from "react";
 import "./contact.css";
 
 const Contact = () => {
+  const [status, setStatus] = useState("Submit")
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value
+    }
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8"
+      },
+      body: JSON.stringify(details)
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  }
   return (
     <section className="contact container section" id='contact'>
       <h2 className="section__title" data-aos="fade-right">How to Contact Me?</h2>
@@ -12,11 +33,12 @@ const Contact = () => {
           <p className="contact__details">Don&apos;t like forms? Send me an Email 👋</p>
         </div>
 
-        <form action="" className="contact__form">
+        <form method="POST" onSubmit={handleSubmit} className="contact__form">
           <div className="contact__form-group">
             <div className="contact__form-div">
               <input
                 type="text"
+                id="name"
                 className="contact__form-input"
                 required placeholder="Your Name"
               />
@@ -25,6 +47,7 @@ const Contact = () => {
             <div className="contact__form-div">
               <input
                 type="email"
+                id="email"
                 className="contact__form-input"
                 required placeholder="Your Email"
               />
@@ -41,15 +64,14 @@ const Contact = () => {
 
             <div className="contact__form-div">
               <textarea
-                name=""
-                id=""
+                id="message"
                 cols="30"
                 rows="10"
                 className="contact__form-input"
                 placeholder="Write your message"
               ></textarea>
             </div>
-            <button className="btn">Send Message</button>
+            <button type="submit" className="btn">{status}</button>
         </form>
       </div>
     </section>
