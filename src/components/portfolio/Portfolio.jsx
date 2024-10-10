7492e8d6476de9167127660910875b13c956c716
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-10 15:01:37 +0800

    Portfolio section added contents, needs to replace some details

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
 import "./portfolio.css";
+import Menu from './Menu';
+import { useState } from "react";
 
 const Portfolio = () => {
+  const [items, setItems] = useState(Menu);
+  const filterItem = (categoryItem) => {
+    const updatedItems = Menu.filter((curElem) => {
+      return curElem.category === categoryItem;
+    });
+
+    setItems(updatedItems);
+  }
+  
   return (
-    <section className="portfolio container section" id='portfolio'>
-      <h2 className="section__title" data-aos="fade-right">Portfolio</h2>
+    <section className="work container section" id='work'>
+      <h2 className="section__title" data-aos="fade-right">Recent Works</h2>
+      <div className="work__filters">
+        <span className="work__item" onClick={() => setItems(Menu)}>Everything</span>
+        <span className="work__item" onClick={() => filterItem("Creative")}>Creative</span>
+        <span className="work__item" onClick={() => filterItem("Art")}>Art</span>
+        <span className="work__item" onClick={() => filterItem("Design")}>Design</span>
+        <span className="work__item" onClick={() => filterItem("Branding")}>Branding</span>
+      </div>
+      <div className="work__container grid">
+        {items.map((elem) => {
+          const { id, image, title, category } = elem;
+          return (
+            <div className="work__card" key={id}>
+              <div className="work__thumbnail">
+                <img src={image} alt="" className="work__img" />
+                <div className="work__mask"></div>
+              </div>
+
+              <span className="work__category">{category}</span>
+              <h3 className="work__title">{title}</h3>
+              <a href="#" className="work__button">
+                <i className="icon-link work__button-icon"></i>
+              </a>
+            </div>
+          )
+        })};
+      </div>
     </section>
   )
 }
