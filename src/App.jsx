02e87220aed149e754e0937ab720cb6323c7a4ff
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-11 16:49:58 +0800

    Deleted the testimonials and pricing part

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import About from "./components/about/About"
 import Contact from "./components/contact/Contact"
 import Home from "./components/home/Home"
 import Portfolio from "./components/portfolio/Portfolio"
-import Pricing from "./components/pricing/Pricing"
 import Resume from "./components/resume/Resume"
 import Services from "./components/services/Services"
 import Sidebar from "./components/sidebar/Sidebar"
-import Testimonials from "./components/testimonials/Testimonials"
 import './App.css';
 import AOS from "aos";
 
@@ -24,8 +22,6 @@ function App() {
         <Services />
         <Resume />
         <Portfolio />
-        <Pricing />
-        <Testimonials />
         <Contact />
       </main>
     </>
