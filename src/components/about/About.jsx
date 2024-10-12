0b83af7095b59aaea48252ec8cf09f389bf029b4
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-12 09:22:17 +0800

    Skills component refactored, in order to be readable

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,7 @@
 import Me from "../../assets/mimic4.jpg";
 import "./about.css";
 import AboutBox from "./AboutBox";
+import { skillsData } from "./Skills";
 
 const About = () => {
   return (
@@ -14,76 +15,28 @@ const About = () => {
 
           <div className="about__info">
             <p className="about__description">Hello! I&apos;m Gio Majadas. A 2nd Year Computer Science Student excelling mostly in Software Development and Web Development. I love learning most of the techy stuff I needed or wanted to upskill in the career path that I want to achieve. My IT career path is focused on Web Development to become a Software Engineer.</p>
-            <a href="src\assets\Gio Majadas (Resume).pdf" className="btn" target="_blank" download={true}>Download CV</a>
+            <a href="src\assets\Gio Majadas (Resume).pdf" className="btn" target="_blank">Download CV</a>
           </div>
 
           <div className="about__skills grid">
-            <div className="skills__data">
-              <div className="skills__titles">
-                <h3 className="skills__name">Front-End Development</h3>
-                <span className="skills__number">62%</span>
-              </div>
+            {skillsData.map((skill, index) => (
+              <div className="skills__data" key={index}>
+                <div className="skills__titles">
+                  <h3 className="skills__name">{skill.name}</h3>
+                  <span className="skills__number">{skill.percentage}%</span>
+                </div>
 
-              <div className="skills__bar">
-                <span className="skills__percentage skill-1"></span>
+                <div className="skills__bar">
+                  <span
+                    className="skills__percentage"
+                    style={{ 
+                      width: `${skill.percentage}%`,
+                      backgroundColor: `${skill.color}` 
+                    }}
+                  ></span>
+                </div>
               </div>
-            </div>
-
-            <div className="skills__data">
-              <div className="skills__titles">
-                <h3 className="skills__name">UI/UX Design</h3>
-                <span className="skills__number">58%</span>
-              </div>
-
-              <div className="skills__bar">
-                <span className="skills__percentage skill-2"></span>
-              </div>
-            </div>
-
-            <div className="skills__data">
-              <div className="skills__titles">
-                <h3 className="skills__name">Database</h3>
-                <span className="skills__number">53%</span>
-              </div>
-
-              <div className="skills__bar">
-                <span className="skills__percentage skill-3"></span>
-              </div>
-            </div>
-
-            <div className="skills__data">
-              <div className="skills__titles">
-                <h3 className="skills__name">Back-End Development</h3>
-                <span className="skills__number">41%</span>
-              </div>
-
-              <div className="skills__bar">
-                <span className="skills__percentage skill-4"></span>
-              </div>
-            </div>
-
-            <div className="skills__data">
-              <div className="skills__titles">
-                <h3 className="skills__name">Full-Stack Development</h3>
-                <span className="skills__number">32%</span>
-              </div>
-
-              <div className="skills__bar">
-                <span className="skills__percentage skill-5"></span>
-              </div>
-            </div>
-
-            <div className="skills__data">
-              <div className="skills__titles">
-                <h3 className="skills__name">Mobile Development</h3>
-                <span className="skills__number">27%</span>
-              </div>
-
-              <div className="skills__bar">
-                <span className="skills__percentage skill-6"></span>
-              </div>
-            </div>
-
+            ))}
           </div>
 
         </div>
---
+++ src/components/about/Skills.jsx
@@ -0,0 +1,32 @@
+export const skillsData = [
+    {
+        name: "Front-End Development",
+        percentage: 62,
+        color: "rgb(255, 209, 92)",
+    },
+    {
+        name: "UI/UX Design",
+        percentage: 58,
+        color: "rgb(255, 0, 255)",
+    },
+    {
+        name: "Database Management",
+        percentage: 53,
+        color: "rgb(0, 0, 255)",
+    },
+    {
+        name: "Back-End Development",
+        percentage: 41,
+        color: "rgb(0, 128, 128)",
+    },
+    {
+        name: "Full-Stack Development",
+        percentage: 32,
+        color: "rgb(128, 20, 126)",
+    },
+    {
+        name: "Mobile App Development",
+        percentage: 27,
+        color: "rgb(26, 208, 68)",
+    },
+];
