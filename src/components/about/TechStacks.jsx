6d2a6a22eb665a5a0376c73c9f1a2325f034d734
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-15 21:49:38 +0800

    Edited contents

--- src/components/about/TechStacks.jsx
+++ src/components/about/TechStacks.jsx
@@ -4,13 +4,9 @@ const TechStacks = () => {
     { name: "HTML", percentage: 45, color: "rgb(241, 101, 41)", },
     { name: "CSS", percentage: 42, color: "rgb(81, 163, 220)", },
     { name: "JavaScript", percentage: 41, color: "rgb(247, 223, 30)", },
-    { name: "PHP", percentage: 46, color: "rgb(138, 148, 191)", },
     { name: "Python", percentage: 52, color: "rgb(255, 193, 7)", },
     { name: "React", percentage: 43, color: "rgb(0, 216, 255)", },
-    { name: "Git", percentage: 46, color: "rgb(240, 60, 46)", },
-    { name: "Flask", percentage: 32, color: "rgb(0, 0, 0)", },
     { name: "MySQL", percentage: 36, color: "rgb(106, 126, 145)", },
-    { name: "Firebase", percentage: 32, color: "rgb(244, 189, 98)", },
   ];
 
   return (
