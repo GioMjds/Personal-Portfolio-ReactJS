6fcbd41ce6d16d250a3ee69c3998f9419e3f072f
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-21 09:36:51 +0800

    Updated home content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         <div className="about-data grid">
 
           <div className="about-info">
-            <p className="about-description">As a student whose having a interest in Web Design, I&apos;m driven to build beautiful and functional websites that captivates users. I&apos;m passionate about creating engaging and intuitive user experiences. I&apos;m also a quick learner and can work well in a team setting, and constantly expanding my knowledge to stay ahead of the curve.</p>
+            <p className="about-description">As a student whose having a interest in Web Design, I&apos;m driven to build beautiful and functional websites that captivates users. I&apos;m passionate about creating, engaging and intuitive user experiences. I&apos;m also a quick learner and can work well in a team setting, and constantly expanding my knowledge to stay ahead of the curve.</p>
             <p className="about-description">Being a student, I&apos;m always seeking new and exciting ways to learn. I&apos;m excited to continue learning and growing as a web developer.</p>
             <a href={Resume} className="btn" target="_blank">Download CV</a>
           </div>
--- src/components/home/Shapes.jsx
+++ src/components/home/Shapes.jsx
@@ -1,22 +1,7 @@
-const Shapes = ({ isDarkMode }) => {
+const Shapes = () => {
   return (
     <div className="shapes">
-      {isDarkMode ? (
-        <svg
-        width="27"
-        height="29"
-        className="shape s1"
-        xmlns="http://www.w3.org/2000/svg"
-        data-aos="fade-down-right"
-      >
-        <path
-          d="M21.15625.60099c4.37954 3.67487 6.46544 9.40612 5.47254 15.03526-.9929 5.62915-4.91339 10.30141-10.2846 12.25672-5.37122 1.9553-11.3776.89631-15.75715-2.77856l2.05692-2.45134c3.50315 2.93948 8.3087 3.78663 12.60572 2.22284 4.297-1.5638 7.43381-5.30209 8.22768-9.80537.79387-4.50328-.8749-9.08872-4.37803-12.02821L21.15625.60099z"
-          fill="#A020F0"
-          fillRule="evenodd"
-        />
-      </svg>
-      ) : (
-        <svg
+      <svg
         width="27"
         height="29"
         className="shape s1"
@@ -29,7 +14,6 @@ const Shapes = ({ isDarkMode }) => {
           fillRule="evenodd"
         />
       </svg>
-      )}
 
       <svg
         width="26"
