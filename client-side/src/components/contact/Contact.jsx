95060df30163d6a33f0ee0cbc8a3356cbcc093eb
Gio-Majadas <sensei.makima55@gmail.com>
2024-10-21 11:11:24 +0800

    Implementing the contact functionality by adding an server-side soon

--- client-side/src/components/contact/Contact.jsx
+++ client-side/src/components/contact/Contact.jsx
@@ -1,41 +1,6 @@
-import { useState } from "react";
 import "./contact.css";
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: '',
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: [e.target.value] });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch('http://localhost:8000/api/send-email', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        alert('Message Sent Successfully!');
-        setFormData({ name: "", email: "", subject: "", message: "" });
-      } else {
-        alert('Error sending message. Please try again.');
-      }
-    } catch (e) {
-      console.log(`Error: ${e}`);
-      alert('Error sending message. Please try again.');
-    }
-  };
-
   return (
     <section className="contact container section" id='contact'>
       <h2 className="section-title" data-aos="fade-right">How to Contact Me?</h2>
@@ -46,14 +11,12 @@ const Contact = () => {
           <p className="contact-details">Don&apos;t like forms? Send me an Email 👋</p>
         </div>
 
-        <form onSubmit={handleSubmit} className="contact-form">
+        <form method="POST" className="contact-form">
           <div className="contact-form-group">
             <div className="contact-form-div">
               <input
                 type="text"
                 id="name"
-                onChange={handleChange}
-                value={formData.name}
                 className="contact-form-input"
                 required placeholder="Your Name"
               />
@@ -63,8 +26,6 @@ const Contact = () => {
               <input
                 type="email"
                 id="email"
-                onChange={handleChange}
-                value={formData.email}
                 className="contact-form-input"
                 required placeholder="Your Email"
               />
@@ -74,8 +35,6 @@ const Contact = () => {
             <div className="contact-form-div contact-form-area">
               <input
                 type="text"
-                onChange={handleChange}
-                value={formData.subject}
                 className="contact-form-input"
                 required placeholder="Your Subject"
               />
@@ -84,8 +43,6 @@ const Contact = () => {
             <div className="contact-form-div">
               <textarea
                 id="message"
-                value={formData.message}
-                onChange={handleChange}
                 cols="30"
                 rows="10"
                 className="contact-form-input"
